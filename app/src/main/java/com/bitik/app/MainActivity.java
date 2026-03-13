a6e2eb7dd59d3b9ba29f2e0c9f230fd101620ce0
Bitik-I <ggvp22858@gmail.com>
2026-03-13 13:39:55 +0300

    MainActivity.java

--- app/src/main/java/com/bitik/app/MainActivity.java
+++ app/src/main/java/com/bitik/app/MainActivity.java
@@ -1,12 +1,15 @@
 package com.bitik.app;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.view.Window;
-import android.view.WindowManager;
 import android.graphics.Color;
 import android.os.Build;
 
@@ -18,7 +21,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Fullscreen / hide title
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             getWindow().setStatusBarColor(Color.parseColor("#080d14"));
@@ -37,6 +39,30 @@ public class MainActivity extends Activity {
 
         webView.setBackgroundColor(Color.parseColor("#080d14"));
         webView.setWebViewClient(new WebViewClient());
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onJsConfirm(WebView view, String url, String message, final JsResult result) {
+                new AlertDialog.Builder(MainActivity.this)
+                    .setMessage(message)
+                    .setPositiveButton(android.R.string.ok, (dialog, which) -> result.confirm())
+                    .setNegativeButton(android.R.string.cancel, (dialog, which) -> result.cancel())
+                    .setCancelable(false)
+                    .show();
+                return true;
+            }
+
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message, final JsResult result) {
+                new AlertDialog.Builder(MainActivity.this)
+                    .setMessage(message)
+                    .setPositiveButton(android.R.string.ok, (dialog, which) -> result.confirm())
+                    .setCancelable(false)
+                    .show();
+                return true;
+            }
+        });
+
         webView.loadUrl("file:///android_asset/index.html");
     }
 
